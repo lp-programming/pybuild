aad7510eb05a6057396e1b51fe64bd7128b5f7d4
perkinslr <logan@github.lp-programming.com>
2025-12-02 20:14:54 -0800

    Add check to verify linker args have compatible ABIs

--- pybuild/library_search.py
+++ pybuild/library_search.py
@@ -1,6 +1,12 @@
+from ._target import colorama
 import subprocess
-
+import enum
 libs = {}
+
+class ABIS(enum.Enum):
+    libcxx = 1
+    libstdcpp = 2
+    C = 3
 
 def find_python(result = []):
     if result:
@@ -15,7 +21,7 @@ def find_python(result = []):
     result.append(r)
     return r
 
-def find_library(name, alt, *extra, pkgconfig=True):
+def find_library(name, alt, *extra, pkgconfig=True, force_abi=ABIS.libcxx):
     result = libs.get(name, None)
     if result:
         return result
@@ -35,6 +41,21 @@ def find_library(name, alt, *extra, pkgconfig=True):
         libs[name] = False
         return False
     libs[name] = args
+    if force_abi:
+        so_a_name = [i for i in args if i.endswith('.so') or i.endswith('.a') or i.startswith('-l')]
+        for libname in so_a_name:
+            p = subprocess.run(["ld", "-t", "-o", "/dev/null", "/dev/stdin", libname], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+            lib_path = p.stdout.decode('utf-8').strip().split('\n')[1]
+            with open(lib_path, 'rb') as lib:
+                data = lib.read()
+                if b'__11' in data and force_abi in (ABIS.libstdcpp, ABIS.C):
+                    print(colorama.Fore.YELLOW, "Found", name, "but it is compiled with libc++ and you requested", force_abi, "only", colorama.Style.RESET_ALL)
+                    libs[name] = False
+                    return False
+                if b'__cxx11' in data and force_abi in (ABIS.libcxx, ABIS.C):
+                    print(colorama.Fore.YELLOW, "Found", name, "but it is compiled with libstdc++ and you requested", force_abi, "only", colorama.Style.RESET_ALL)
+                    libs[name] = False
+                    return False
     return args
 
 
